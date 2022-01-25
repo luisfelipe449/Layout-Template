103eb837e9bdf707dd385e0723470ee7c3f9935a
luisfelipe449 <61158230+luisfelipe449@users.noreply.github.com>
2022-01-24 22:25:52 -0400

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { FullwidthComponent } from './layouts/fullwidth/fullwidth.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component: DefaultComponent,
+  children: [{
+    path:'',
+    component: HomeComponent,
+  },{
+    path: 'posts',
+    component: PostsComponent,
+  },{
+    path:'',
+    component:FullwidthComponent,
+    children:[{
+      path:'login',
+      component: LoginComponent,
+    }]
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { FullwidthModule } from './layouts/fullwidth/fullwidth.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+      
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DefaultModule,
+    FullwidthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

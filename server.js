a5629f38ff0fc2f953760951449876f0108644f9
luisfelipe449 <1luisfelipe2@gmail.com>
2022-02-01 14:19:47 -0400

    Servidor heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/template-2'));
+
+app.get('/*', (req,res) =>{
+    res.sendFile(__dirname + '/dist/template-2/index.html');
+});
+
+app.listen(PORT, () =>{
+    console.log('Servidor iniciado na porta' + PORT);
+})

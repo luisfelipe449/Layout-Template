cc1d4f9b2e6b7ba0917c7e5144ebff8ec1141b92
luisfelipe449 <1luisfelipe2@gmail.com>
2022-02-01 13:03:14 -0400

    image-slider alterado

--- src/app/modules/artesanatos/japamalas/japamalas.component.ts
+++ src/app/modules/artesanatos/japamalas/japamalas.component.ts
@@ -12,18 +12,37 @@ export class JapamalasComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  imageCollection: Array<object> = [{
+  imageCollection: Array<object> = 
+  [
+    {
     image: '../../../../assets/japamala1.jpg',
     thumbImage: '../../../../assets/japamala1.jpg',
-    alt: 'alt of image',
-    title: 'title of image'
+    alt: 'Japamala 1',
+    title: '',
 }, {
     image: '../../../../assets/japamala2.webp',
-    thumbImage: '../../../../assets/japamala2.webp', // Support base64 image
-    title: 'Image title', //Optional: You can use this key if want to show image with title
-    alt: 'Image alt', //Optional: You can use this key if want to show image with alt
-    order: 2 //Optional: if you pass this key then slider images will be arrange according @input: slideOrderType
-}
+    thumbImage: '../../../../assets/japamala2.webp',
+    title: '',
+    alt: 'foto-japamala'
+},
+{
+    image: '../../../../assets/japamala3.png',
+    thumbImage: '../../../../assets/japamala3.png',
+    title: '',
+    alt: 'foto-japamala'
+},
+{
+    image: '../../../../assets/japamala4.webp',
+    thumbImage: '../../../../assets/japamala4.webp',
+    title: '',
+    alt: 'foto-japamala'
+},
+{
+    image: '../../../../assets/japamal5.jpg',
+    thumbImage: '../../../../assets/japamal5.jpg',
+    title: '',
+    alt: 'foto-japamala'
+},
 ];
 
 
